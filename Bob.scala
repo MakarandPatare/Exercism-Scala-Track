90bc9d3e12aebca4b6c99a6ac85553bc1abfab69
Makarand Patare <makarand.patare@springernature.com>
2019-04-22 15:57:14 +0530

    Update Bob.scala

--- Bob.scala
+++ Bob.scala
@@ -1,30 +1,11 @@
 object Bob {
-
-  def isEmpty(statement: String) = statement.trim.isEmpty
-
-  def isShout(statement: String) = {
-    statement == statement.toUpperCase && statement.exists(_.isLetter)
-  }
-
-  def isQuestion(statement: String) = statement.trim.endsWith("?")
-
-  def isForcefulQuestion(statement: String): Boolean = {
-    statement == statement.toUpperCase && statement.exists(_.isLetter) && statement.trim.endsWith("?")
-  }
-
   def response(statement: String): String = {
-    val speak: String = {
-      if (isEmpty(statement))
-        "Fine. Be that way!"
-      else if (isForcefulQuestion(statement))
-        "Calm down, I know what I'm doing!"
-      else if (isShout(statement))
-        "Whoa, chill out!"
-      else if (isQuestion(statement))
-        "Sure."
-      else
-        "Whatever."
-    }
-    speak
+      statement match {
+        case s if s.endsWith("?") && s.contentEquals(s.toUpperCase()) && "[a-zA-Z]".r.findFirstMatchIn(s).nonEmpty => "Calm down, I know what I'm doing!"
+        case s if s.trim.endsWith("?") => "Sure."
+        case s if s.contentEquals(s.toUpperCase()) && "[a-zA-Z]".r.findFirstMatchIn(s).nonEmpty => "Whoa, chill out!"
+        case s if s.trim.isEmpty => "Fine. Be that way!"
+        case _ => "Whatever."
+      }
   }
 }
